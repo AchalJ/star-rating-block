fc8e7d62719ddd684761c2a63d2c946dcf078e42
Achal Jain <achal@ideaboxcreations.com>
2020-09-09 12:10:13 +0530

    Updated package, fixed errors, and added WP assets

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-polyfill'), 'version' => '30dc94da3b2cd16604663e8db25597c3');
+<?php return array('dependencies' => array('react', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-i18n', 'wp-polyfill'), 'version' => '13127459f4a272bf2fc3abc0aecc8fe4');
